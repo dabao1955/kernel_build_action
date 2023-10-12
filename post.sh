a9c34612191e12cc8f1e795ffb0e743041ecb966
dabao1955 <Admin@qw5d.onmicrosoft.com>
2023-10-12 21:37:26 +0800

    Update post.sh

--- post.sh
+++ post.sh
@@ -6,12 +6,6 @@ if [ -f /usr/bin/ccache ]; then
    export USE_CCACHE=0
 fi
 
-if [ -d out ]; then
-   rm -rf out
-else
-   make ARCH=arm64 clean
-   make ARCH=arm64 mrproper
-fi
 
 if [ -d $HOME/clang ]; then
     $HOME/clang/bin/clang --version
