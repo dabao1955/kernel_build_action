d3e2539b210989ad39a18725a228e879310a9a93
dabao1955 <dabao1955@163.com>
2024-08-09 17:21:44 +0800

    actions: feat(KernelSU): Initialize gki check script
    Signed-off-by: dabao1955 <dabao1955@163.com>

---
+++ kernelsu/check_gki.pl
@@ -0,0 +1,30 @@
+#!/bin/perl -w
+
+use strict;
+use warnings;
+
+open(my $fh, '<', 'Makefile') or die "Can not open Makefil: $!";
+my $content = do { local $/; <$fh> };
+close($fh);
+
+my ($level) = $content =~ /VERSION\s*=\s*(\d+)/;
+my ($sublevel) = $content =~ /PATCHLEVEL\s*=\s*(\d+)/;
+my $command = "curl -SsL https://github.com/dabao1955/kernel_build_action/raw/main/kernelsu/ksupatch.sh | bash"
+
+die "Can not find PATCHLEVEL in Makefile." unless defined $sublevel;
+
+print "SUBLEVEL: $sublevel\n";
+
+my $gkilevel = 10;
+my $gkiversion = 5;
+
+if ($level > $gkiversion) {
+	if ($sublevel > $gkilevel) {
+		print "nothing todo.";
+	} else {
+		system($command);
+	}
+} else {
+	system($command);
+}
+
